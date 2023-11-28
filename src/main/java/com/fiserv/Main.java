322ca756ffd35be39f5a4e3a4071b929372164ad
juan.bianchini <juan.bianchini@bp-4.com>
2023-11-28 12:49:30 -0300

    Se utiliza certificado p 12 para pegarle a la API de máster com Se agrega el logger . La prueba es exitosa

--- src/main/java/com/fiserv/Main.java
+++ src/main/java/com/fiserv/Main.java
@@ -1,31 +1,47 @@
 package com.fiserv;
 
 import bp4.mastercom_api_client.ApiClient;
+import bp4.mastercom_api_client.ApiException;
+import bp4.mastercom_api_client.api.TransactionsApi;
+import bp4.mastercom_api_client.model.TransactionSearchRequest;
+import bp4.mastercom_api_client.model.TransactionSummary;
+import com.mastercard.developer.encryption.EncryptionException;
+import com.mastercard.developer.encryption.FieldLevelEncryptionConfig;
+import com.mastercard.developer.encryption.FieldLevelEncryptionConfigBuilder;
+import com.mastercard.developer.interceptors.OkHttpEncryptionInterceptor;
 import com.mastercard.developer.interceptors.OkHttpOAuth1Interceptor;
 import com.mastercard.developer.utils.AuthenticationUtils;
-import okhttp3.OkHttpClient;
 
 import java.io.IOException;
-import java.net.http.HttpClient;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.UnrecoverableKeyException;
+import java.security.*;
 import java.security.cert.CertificateException;
 
+import com.mastercard.developer.utils.EncryptionUtils;
+import okhttp3.OkHttpClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
-    public static void main(String[] args) {
+
+    private static Logger logger = LoggerFactory.getLogger(Main.class.getName());
+
+    public static void main(String[] args) throws IOException, GeneralSecurityException, EncryptionException, ApiException {
         ApiClient client = new ApiClient();
         OkHttpClient okHttpClient = client.getHttpClient();
 
         OkHttpClient.Builder httpClientBuilder = okHttpClient.newBuilder();
 
         // Configure the Mastercard service URL
-        client.setBasePath("https://sandbox.api.mastercard.com/mdes");
+        client.setBasePath("https://sandbox.api.mastercard.com/mastercom");
         // Load the signing key
         PrivateKey signingKey = null;
         try {
-            signingKey = AuthenticationUtils.loadSigningKey("./path/to/your/signing-key.p12", "keyalias", "keystorepassword");
+            signingKey =
+                    AuthenticationUtils.loadSigningKey(
+                            "src/main/resources/Mastercom_test_client-sandbox.p12",
+                            "keyalias",
+                            "keystorepassword"
+                    );
         } catch (IOException e) {
             throw new RuntimeException(e);
         } catch (KeyStoreException e) {
@@ -37,10 +53,21 @@ public class Main {
         } catch (UnrecoverableKeyException e) {
             throw new RuntimeException(e);
         }
-        // Add the interceptor code responsible for signing HTTP requests
-        httpClientBuilder.addInterceptor(new OkHttpOAuth1Interceptor("000000000000000000000000000000000000000000000000!000000000000000000000000000000000000000000000000", signingKey));
 
+        // Add the interceptor code responsible for signing HTTP requests
+        httpClientBuilder.addInterceptor(new OkHttpOAuth1Interceptor("tOtuKPlv5pUb0d-iNWZf0m7zxTZd6f_mcGKNHg-P2b774e70!f1e33cbc653249b397e7b65476f8710c0000000000000000", signingKey));
+        // ...
         client.setHttpClient(httpClientBuilder.build());
 
+        TransactionsApi api = new TransactionsApi(client);
+
+        TransactionSearchRequest searchRequest = new TransactionSearchRequest()
+                .acquirerRefNumber("12345678901234567890123")
+                .tranStartDate("2023-01-01")
+                .tranEndDate("2023-01-31");
+
+        TransactionSummary searchResponse = api.transactionSearch(searchRequest);
+
+        logger.info("logger" +searchResponse.getAuthorizationSummary().toString());
     }
 }
